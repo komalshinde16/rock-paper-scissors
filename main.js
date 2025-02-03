ee9fe2060785bce483f5232533034b631607791c
KOMAL <komalambadasshinde@gmail.com>
2025-02-03 11:09:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+const options = ["rock", "paper", "scissors"];
+
+function playGame(){
+   let usersChoice1 = prompt("enter your choice");
+   console.log(usersChoice1);   //r,p,s
+
+   
+ if(
+    usersChoice!= "rock"&&
+    usersChoice!= "paper"&&
+    usersChoice!= "scissors"
+ ){
+    alert("enter proper input");
+    playGame();
+    return;
+ }
+
+
+
+
+
+if(usersChoice1===null || usersChoice1===""){
+    alert("enter input bro"); 
+    playGame();
+    return;
+    
+}
+
+let usersChoice = usersChoice1.trim().toLowerCase();
+
+   //computer choice
+
+   let randomIndex= Math.floor(Math.random()* options.length);
+   const computersChoice = options[randomIndex];
+   
+   console.log("users choice", usersChoice);
+console.log("comp choice", computersChoice);
+
+   //check who won this round
+
+   //wehn user will win
+
+   if((usersChoice==="rock" && computersChoice==="scissors") ||  
+     (usersChoice==="paper" && computersChoice==="rock")||
+   (usersChoice==="scissors" && computersChoice==="paper")
+){
+    alert("congo user.. u won!!!");
+
+} else if 
+    ((usersChoice==="scissors" && computersChoice==="rock") ||  
+(usersChoice==="rock" && computersChoice==="paper")||
+(usersChoice==="paper" && computersChoice==="scissors")
+) {
+    alert("congrats compy..u won!!!");
+
+} else {
+    alert("draw, i am sorry : (");
+}
+
+}
+
+
+
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,74 @@
+const options = ["rock", "paper", "scissors"];
+const userChoiceContainer = document.getElementById("user-choice");
+const computerChoiceContainer = document.getElementById("computer-choice");
+const resultContainer = document.getElementById("result");
+
+function selectRock() {
+  // userChoice = "rock"
+  const userChioce = "rock";
+
+  userChoiceContainer.innerText = "Your choice : Rock (🪨)";
+
+  // computer choice --> generate
+
+  const computersChoice = getComputersChoice();
+
+  computerChoiceContainer.innerHTML = "Computers choice : " + computersChoice;
+
+  // check who won
+  checkWhoWon(userChioce, computersChoice);
+}
+
+function selectPaper() {
+  userChoiceContainer.innerText = "Your choice : Paper (📄)";
+  // userChoice = "rock"
+  const userChioce = "paper";
+
+  // computer choice --> generate
+
+  const computersChoice = getComputersChoice();
+
+  computerChoiceContainer.innerHTML = "Computers choice : " + computersChoice;
+
+  // check who won
+  checkWhoWon(userChioce, computersChoice);
+}
+
+function selectScissors() {
+  // userChioce = "scissors"
+  userChoiceContainer.innerText = "Your choice : Scissors (✂️)";
+  // userChoice = "rock"
+  const userChioce = "scissors";
+
+  // computer choice --> generate
+
+  const computersChoice = getComputersChoice();
+
+  computerChoiceContainer.innerHTML = "Computers choice : " + computersChoice;
+
+  // check who won
+  checkWhoWon(userChioce, computersChoice);
+}
+
+function getComputersChoice() {
+  let randomIndex = Math.floor(Math.random() * options.length); // 0 , 1, 2
+  return options[randomIndex];
+}
+
+function checkWhoWon(usersChioce, computersChoice) {
+  if (
+    (usersChioce === "rock" && computersChoice === "scissors") ||
+    (usersChioce === "paper" && computersChoice === "rock") ||
+    (usersChioce === "scissors" && computersChoice === "paper")
+  ) {
+    resultContainer.innerHTML = "Result: Cong. You won!!";
+  } else if (
+    (computersChoice === "rock" && usersChioce === "scissors") ||
+    (computersChoice === "paper" && usersChioce === "rock") ||
+    (computersChoice === "scissors" && usersChioce === "paper")
+  ) {
+    resultContainer.innerHTML = "Result : You Lost..";
+  } else {
+    resultContainer.innerHTML = "Result : Draw";
+  }
+}
